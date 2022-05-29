fa10e9e13ee9a5649dbb53bb973b61f33839e38f
Diego Ochoa <dvendeb45@gmail.com>
2022-05-29 16:17:28 -0500

    project final

---
+++ Root13/Root13/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace Root13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.txtB_String = new System.Windows.Forms.TextBox();
+            this.btn_encrypting = new System.Windows.Forms.Button();
+            this.txtB_TextEngrypting = new System.Windows.Forms.TextBox();
+            this.txtB_StringEncrypting = new System.Windows.Forms.TextBox();
+            this.btn_Desencrypting = new System.Windows.Forms.Button();
+            this.txtB_TextDesencrypting = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Font = new System.Drawing.Font("Stencil", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_title.Location = new System.Drawing.Point(289, 9);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(163, 42);
+            this.lbl_title.TabIndex = 0;
+            this.lbl_title.Text = "ROOT 13";
+            // 
+            // txtB_String
+            // 
+            this.txtB_String.Location = new System.Drawing.Point(34, 68);
+            this.txtB_String.Name = "txtB_String";
+            this.txtB_String.Size = new System.Drawing.Size(554, 20);
+            this.txtB_String.TabIndex = 1;
+            // 
+            // btn_encrypting
+            // 
+            this.btn_encrypting.Location = new System.Drawing.Point(618, 65);
+            this.btn_encrypting.Name = "btn_encrypting";
+            this.btn_encrypting.Size = new System.Drawing.Size(75, 23);
+            this.btn_encrypting.TabIndex = 2;
+            this.btn_encrypting.Text = "Encriptar";
+            this.btn_encrypting.UseVisualStyleBackColor = true;
+            this.btn_encrypting.Click += new System.EventHandler(this.btn_encrypting_Click);
+            // 
+            // txtB_TextEngrypting
+            // 
+            this.txtB_TextEngrypting.Enabled = false;
+            this.txtB_TextEngrypting.Location = new System.Drawing.Point(34, 103);
+            this.txtB_TextEngrypting.Multiline = true;
+            this.txtB_TextEngrypting.Name = "txtB_TextEngrypting";
+            this.txtB_TextEngrypting.Size = new System.Drawing.Size(659, 157);
+            this.txtB_TextEngrypting.TabIndex = 3;
+            // 
+            // txtB_StringEncrypting
+            // 
+            this.txtB_StringEncrypting.Location = new System.Drawing.Point(34, 296);
+            this.txtB_StringEncrypting.Name = "txtB_StringEncrypting";
+            this.txtB_StringEncrypting.Size = new System.Drawing.Size(554, 20);
+            this.txtB_StringEncrypting.TabIndex = 4;
+            // 
+            // btn_Desencrypting
+            // 
+            this.btn_Desencrypting.Location = new System.Drawing.Point(618, 296);
+            this.btn_Desencrypting.Name = "btn_Desencrypting";
+            this.btn_Desencrypting.Size = new System.Drawing.Size(75, 23);
+            this.btn_Desencrypting.TabIndex = 5;
+            this.btn_Desencrypting.Text = "Desencriptar";
+            this.btn_Desencrypting.UseVisualStyleBackColor = true;
+            this.btn_Desencrypting.Click += new System.EventHandler(this.btn_Desencrypting_Click);
+            // 
+            // txtB_TextDesencrypting
+            // 
+            this.txtB_TextDesencrypting.Enabled = false;
+            this.txtB_TextDesencrypting.Location = new System.Drawing.Point(34, 337);
+            this.txtB_TextDesencrypting.Multiline = true;
+            this.txtB_TextDesencrypting.Name = "txtB_TextDesencrypting";
+            this.txtB_TextDesencrypting.Size = new System.Drawing.Size(659, 171);
+            this.txtB_TextDesencrypting.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(734, 566);
+            this.Controls.Add(this.txtB_TextDesencrypting);
+            this.Controls.Add(this.btn_Desencrypting);
+            this.Controls.Add(this.txtB_StringEncrypting);
+            this.Controls.Add(this.txtB_TextEngrypting);
+            this.Controls.Add(this.btn_encrypting);
+            this.Controls.Add(this.txtB_String);
+            this.Controls.Add(this.lbl_title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.TextBox txtB_String;
+        private System.Windows.Forms.Button btn_encrypting;
+        private System.Windows.Forms.TextBox txtB_TextEngrypting;
+        private System.Windows.Forms.TextBox txtB_StringEncrypting;
+        private System.Windows.Forms.Button btn_Desencrypting;
+        private System.Windows.Forms.TextBox txtB_TextDesencrypting;
+    }
+}
+
---
+++ Root13/Root13/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Root13
+{
+    public partial class Form1 : Form
+    {
+        public char[] arrayRoot = {' ','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M' };
+        public char [] arrayRootOne={' ','N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        public String stringText;
+        public String stringEncrypting;
+        public char[] arrayStringText;
+        public char[] arrayEncrypting;
+        public char[] arrayTextDesencrypting;
+        public char[] arrayDesencrypting;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_encrypting_Click(object sender, EventArgs e)
+        {
+            encrypting();
+            textEncrypting();
+        }
+
+        public void encrypting()
+        {
+            stringText = txtB_String.Text.ToUpper();
+            arrayStringText = stringText.ToArray();
+            arrayEncrypting = new char[arrayStringText.Length];
+            for (int j = 0; j < arrayStringText.Length; j++)
+            {
+                for (int i = 0; i < arrayRoot.Length; i++)
+                {
+                    if (arrayStringText[j]==arrayRoot[i])
+                    {
+                        arrayEncrypting[j] = arrayRootOne[i];
+                    }else if (arrayStringText[j] == arrayRootOne[i])
+                    {
+                        arrayEncrypting[j] = arrayRoot[i];
+                    }
+                } 
+            }
+        }
+
+        public void textEncrypting()
+        {
+            txtB_TextEngrypting.Text = "";
+            for (int i = 0; i < arrayEncrypting.Length; i++)
+            {
+                txtB_TextEngrypting.Text += arrayEncrypting[i];
+            }
+        }
+
+        private void btn_Desencrypting_Click(object sender, EventArgs e)
+        {
+            desencrypting();
+            textDesencrypting();
+
+        }
+
+        public void desencrypting()
+        {
+            stringEncrypting = txtB_StringEncrypting.Text.ToUpper();
+            arrayTextDesencrypting = stringEncrypting.ToCharArray();
+            arrayDesencrypting = new char[arrayTextDesencrypting.Length];
+            for (int j = 0; j < arrayTextDesencrypting.Length; j++)
+            {
+                for (int i = 0; i < arrayRoot.Length; i++)
+                {
+                    if (arrayTextDesencrypting[j] == arrayRoot[i])
+                    {
+                        arrayDesencrypting[j] = arrayRootOne[i];
+                    }
+                    else if (arrayTextDesencrypting[j] == arrayRootOne[i])
+                    {
+                        arrayDesencrypting[j] = arrayRoot[i];
+                    }
+                }
+            }
+        }
+        public void textDesencrypting()
+        {
+            txtB_TextDesencrypting.Text = "";
+            for (int i = 0; i < arrayDesencrypting.Length; i++)
+            {
+                txtB_TextDesencrypting.Text += arrayDesencrypting[i];
+            }
+        }
+    }
+}
